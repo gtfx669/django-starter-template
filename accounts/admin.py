9ea81ab27e8fd44059f2eb730e1f9ef627a68b5f
Leon Rashkovsky <leon.rashkovsky@gmail.com>
2024-10-19 13:23:18 +0300

    initial

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,24 @@
+from typing import ClassVar
+
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    model = CustomUser
+    list_display: ClassVar[list[str]] = [
+        "email",
+        "username",
+        "is_superuser",
+    ]
+
 
 admin.site.register(CustomUser, UserAdmin)
