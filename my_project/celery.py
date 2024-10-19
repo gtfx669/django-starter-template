9365370a0256075e20177ae60848f6cc2522c335
Leon Rashkovsky <leon.rashkovsky@gmail.com>
2024-10-19 19:28:29 +0300

    initial

--- my_project/celery.py
+++ my_project/celery.py
@@ -2,9 +2,9 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
-app = Celery("my_project")
+app = Celery("{{ project_name }}")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
